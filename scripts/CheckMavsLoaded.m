871ea71b72e331b4a1751dbb5717ecf31049c763
Chris Goodin <cgoodin@cavs.msstate.edu>
2023-09-22 16:40:15 -0500

    Another crack at loading the dll

--- scripts/CheckMavsLoaded.m
+++ scripts/CheckMavsLoaded.m
@@ -1,19 +1,18 @@
 function CheckMavsLoaded()
 
 fname = '..\mavs\dll';
-file = java.io.File(fname);
-canon_path = file.getCanonicalPath();
+curr_dir = pwd;
+canon_path = strcat(curr_dir,strcat('\',fname));
 full_path = string(canon_path);
 current_path = string(getenv('PATH'));
 
 if ~contains(current_path, full_path)
     sprintf('Adding MAVS DLL to MATLAB PATH')
-    mavs_dll_path = full_path; %'..\mavs\dll';
+    mavs_dll_path = full_path; 
     addpath(mavs_dll_path);
     syspath = getenv('PATH');
     dllPath = char(mavs_dll_path);
     setenv('PATH',[dllPath pathsep syspath]);
-%     %savepath;
 end
 
 end
