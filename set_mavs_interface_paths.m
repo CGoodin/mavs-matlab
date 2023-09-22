266b0d341bbc154c4c4f76726d3e9ffcf0701f20
Chris Goodin <cgoodin@cavs.msstate.edu>
2023-09-22 15:13:36 -0500

    Changed forward slashes to backslashes

--- set_mavs_interface_paths.m
+++ set_mavs_interface_paths.m
@@ -1,5 +1,5 @@
 function set_mavs_interface_paths()
-    mavs_dll_path = './mavs/dll';
+    mavs_dll_path = '.\mavs\dll';
     addpath(mavs_dll_path);
     syspath = getenv('PATH');
     dllPath = mavs_dll_path;
